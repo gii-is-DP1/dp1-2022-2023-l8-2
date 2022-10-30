3d95e0d4e1fd49d6f6b1a367c140e49fe68cf20c
Juan Lopez <juanquiroslopez@gmail.com>
2022-10-30 11:14:08 +0100

    fix-no petclinic references in package names

--- src/main/java/org/springframework/samples/sieteislas/statistics/playerStatistics/playerStatistics.java
+++ src/main/java/org/springframework/samples/sieteislas/statistics/playerStatistics/playerStatistics.java
@@ -1,8 +1,18 @@
 package org.springframework.samples.sieteislas.statistics.playerStatistics;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.samples.sieteislas.card.Card;
 import org.springframework.samples.sieteislas.model.BaseEntity;
+/* import org.springframework.samples.sieteislas.player.Player; */
+import org.springframework.samples.sieteislas.user.User;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,12 +20,29 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name="playerStatistics")
-public class playerStatistics extends BaseEntity{
+public class PlayerStatistics extends BaseEntity{
 
+	@NotNull
+	@Column(name = "total_points")
     private Integer totalPoints;
+	
+	@NotNull
+	@Column(name = "time_played")
     private Double timePlayed;
+	
+	@NotNull
+	@Column(name = "games_played")
     private Integer gamesPlayed;
+	
+	@NotNull
+	@Column(name = "games_won")
     private Integer gamesWon;
-    private String favCard; //String temporal until Card is implemented.
+	
+/* 	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "player_id")
+	private Player player;
+	 */
+	
+    //private Card favCard; 
 
 }
