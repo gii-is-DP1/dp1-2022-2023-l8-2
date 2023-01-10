b087632a93eecdfddbfa403864eb9c6de6ff1652
joscasvaz <jicastrotic99@gmail.com>
2023-01-10 20:37:17 +0100

    fix: assertions and transactional annotations added.
    Still not working. Other minor changes added.

--- src/test/java/org/springframework/samples/sieteislas/TestDice.java
+++ src/test/java/org/springframework/samples/sieteislas/TestDice.java
@@ -1,49 +1,76 @@
 package org.springframework.samples.sieteislas;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.sieteislas.game.Game;
+import org.springframework.samples.sieteislas.game.GameRepository;
 import org.springframework.samples.sieteislas.game.GameService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@ExtendWith(MockitoExtension.class)
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@Transactional
 public class TestDice {
 	
-	@MockBean
-	Game game;
+	@Mock
+	private Game game;
 	
-	@MockBean
-	GameService gameService;
+	@Mock
+	private GameRepository gameRepo;
+	
+	protected GameService gameService;
+	
+	@BeforeEach
+	private void setup() {
+		gameService = new GameService(gameRepo,
+				null, null, null, null, null, null);
+	}
 	
 	@Test
 	public void testDice() throws Exception {
-		
+		testDiceRolls();
+	}
+	
+	@Transactional
+	public void testDiceRolls() throws Exception {
 		List<Integer> diceRolls = new ArrayList<>();
 		
 		for(int i = 0; i < 99; i++) {
 			
 			gameService.rollDice(game);
-			int diceRoll = game.getDiceRoll();
+			diceRolls.add(game.getDiceRoll());
 			
-			diceRolls.add(diceRoll);
+			//gameService.toggleHasRolledDice(game, false);
 		}
 		
-		if(diceRolls.stream().anyMatch(x-> x < 0 || 5 < x))
-			throw new Exception("Dice is returning illegal values.");
-		
 		Map<Integer, Long> mem = diceRolls.stream()
-			.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+			.collect(Collectors.groupingBy(Function.identity(),
+					Collectors.counting()));
+		
+		//assertTrue(mem.entrySet().stream().anyMatch(x-> x.getValue() < 50));
+		assertFalse(diceRolls.stream().anyMatch(x-> x < 0 || 5 < x));
 		
 		if(mem.entrySet().stream().anyMatch(x-> 50 < x.getValue()))
-			throw new Exception("Dice is not random: " + mem);
+			throw new Exception(""+mem);
 	}
 	
 	
