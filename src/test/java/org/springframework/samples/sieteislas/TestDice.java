c10fb9e2f01512298d261c31f4c68c8d6a67da85
joscasvaz <jicastrotic99@gmail.com>
2023-01-10 20:43:31 +0100

    fix: minor fixes.
    Still throwing the exception.

--- src/test/java/org/springframework/samples/sieteislas/TestDice.java
+++ src/test/java/org/springframework/samples/sieteislas/TestDice.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.sieteislas;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,26 +8,24 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.sieteislas.game.Game;
 import org.springframework.samples.sieteislas.game.GameRepository;
 import org.springframework.samples.sieteislas.game.GameService;
 import org.springframework.stereotype.Service;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 @Transactional
+@Sql(scripts = "/test.sql" ,executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class TestDice {
 	
 	@Mock
@@ -37,13 +34,8 @@ public class TestDice {
 	@Mock
 	private GameRepository gameRepo;
 	
-	protected GameService gameService;
-	
-	@BeforeEach
-	private void setup() {
-		gameService = new GameService(gameRepo,
-				null, null, null, null, null, null);
-	}
+	@InjectMocks
+	private GameService gameService;
 	
 	@Test
 	public void testDice() throws Exception {
